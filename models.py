9e11be03f49e35e68daa234ec1d0806e82718fbf
RoCry <crysheen@gmail.com>
2025-04-11 16:39:57 +0800

    Implement mindmap generation in main.py by fetching top HackerNews stories and their comments; create HTML mindmaps and a JSON feed. Update FeedItem and JSONFeed models for enhanced functionality and serialization. Configure GitHub Actions to set up Node.js and install markmap-cli for mindmap generation.

--- models.py
+++ models.py
@@ -1,38 +1,46 @@
 import json
+import asyncio
 from datetime import datetime
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, Field, field_serializer
 
 # https://www.jsonfeed.org/version/1.1/
 
 
 class FeedItem(BaseModel):
     id: str
-    url: HttpUrl
+    url: Optional[str] = None
     title: str
     content_html: str
-    image: HttpUrl
     date_published: datetime
-    date_modified: datetime
-    _social_mindmap: Dict[str, Any]
-
-
-class Feed(BaseModel):
+    external_url: Optional[str] = None
+    image: Optional[HttpUrl] = None
+    
+    @field_serializer('date_published')
+    def serialize_dt(self, dt: datetime):
+        return dt.isoformat()
+    
+class JSONFeed(BaseModel):
     version: str = "https://jsonfeed.org/version/1.1"
     title: str
-    home_page_url: HttpUrl
-    feed_url: HttpUrl
-    description: str
-    items: List[FeedItem] = []
-
+    home_page_url: Optional[str] = None
+    feed_url: Optional[str] = None
+    description: Optional[str] = None
+    icon: Optional[HttpUrl] = None
+    favicon: Optional[HttpUrl] = None
+    items: List[FeedItem] = Field(default_factory=list)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    
+    @field_serializer('updated_at')
+    def serialize_dt(self, dt: datetime):
+        return dt.isoformat()
+    
     def save_to_json(self, filepath: str) -> None:
         with open(filepath, "w", encoding="utf-8") as f:
-            json.dump(
-                self.model_dump(exclude_none=True), f, ensure_ascii=False, indent=2
-            )
+            f.write(self.model_dump_json(indent=2))
 
     @classmethod
-    def load_from_json(cls, filepath: str) -> "Feed":
+    def load_from_json(cls, filepath: str) -> "JSONFeed":
         with open(filepath, "r", encoding="utf-8") as f:
             return cls.model_validate_json(f.read())
